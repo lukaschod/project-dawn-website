5dfd9159cc23175d6eb4826e712eee9a8b6ad99a
Lukas Chodosevicius <lukaschodosevicius@gmail.com>
2025-08-23 09:58:33 +0300

    Update

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Project Dawn';
-export const SITE_DESCRIPTION = '';
+export const SITE_DESCRIPTION = 'At Project Dawn, we create game development tools, built by developers for developers.';
